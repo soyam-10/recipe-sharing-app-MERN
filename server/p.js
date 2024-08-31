e8fdf56b915d2da67cd00602d49ffe52e97e7f61
soyam-10 <sharmasoyam121@gmail.com>
2024-08-31 23:18:20 +0545

    ;)

--- server/p.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-
-const newPassword = '123456789'; // Replace with the new password
-bcrypt.hash(newPassword, 10, (err, hash) => {
-  if (err) throw err;
-  console.log('New Password Hash:', hash);
-});

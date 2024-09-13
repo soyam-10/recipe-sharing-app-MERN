8cc57e8eb6b5d663981a5bcd6c16176dcbaf7e17
soyam-10 <sharmasoyam121@gmail.com>
2024-09-13 08:51:57 +0545

    added localhost to Cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware setup
 app.use(express.json());
 app.use(cors({
-  origin: ['https://hamro-recipe.vercel.app'],
+  origin: ['https://hamro-recipe.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
   credentials: true, // Enable Access-Control-Allow-Credentials
   optionsSuccessStatus: 204 // Some legacy browsers choke on 204
